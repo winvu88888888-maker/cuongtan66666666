5f883a2e2c95854e647e26aaef5fb804b263dab4
winvu88888888 <winvu88888888@gmail.com>
2026-04-23 10:23:04 +0700

    V42.5b: Bo sung 43+ sao Tu Vi - Khoi/Viet, Hoa/Linh, Khong/Kiep, Ma, Dao Hoa, Hong Loan, Truong Sinh 12

--- tu_vi.py
+++ tu_vi.py
@@ -130,27 +130,119 @@ def lap_la_so(nam_sinh, thang_am, ngay_am, gio_idx, gioi_tinh='nam'):
             if cung_data["chi_idx"] == pos:
                 cung_data["chinh_tinh"].append(star)
 
-    # Phụ tinh cơ bản
-    # Lộc Tồn theo Can năm
-    loc_ton_map = [2,3,5,6,5,6,8,9,11,0]  # Giáp=Dần, Ất=Mão,...
+    # Dương/Âm nam (cần cho Trường Sinh + Đại Hạn)
+    duong_nam = (gioi_tinh == 'nam' and can_idx % 2 == 0) or (gioi_tinh == 'nu' and can_idx % 2 == 1)
+
+    # Phụ tinh — Lộc Tồn + Kình/Đà
+    loc_ton_map = [2,3,5,6,5,6,8,9,11,0]
     loc_ton_pos = loc_ton_map[can_idx]
-    # Kình Dương = Lộc Tồn + 1, Đà La = Lộc Tồn - 1
     kinh_pos = (loc_ton_pos + 1) % 12
     da_la_pos = (loc_ton_pos - 1) % 12
 
-    # Văn Xương theo giờ, Văn Khúc theo giờ
+    # Văn Xương/Khúc theo giờ
     van_xuong_pos = (10 - gio_idx) % 12
     van_khuc_pos = (gio_idx + 4) % 12
 
-    # Tả Phụ theo tháng, Hữu Bật theo tháng
+    # Tả Phụ/Hữu Bật theo tháng
     ta_phu_pos = (thang_am + 3) % 12
     huu_bat_pos = (11 - thang_am) % 12
 
+    # Thiên Khôi/Thiên Việt (Quý Nhân) theo Can năm
+    khoi_map = [1,0,11,11,1,0,1,6,3,3]  # Giáp=Sửu,...
+    viet_map = [7,8,9,9,7,8,7,2,5,5]
+    thien_khoi_pos = khoi_map[can_idx]
+    thien_viet_pos = viet_map[can_idx]
+
+    # Hỏa Tinh theo Chi năm + giờ
+    chi_nam_idx = CHI.index(chi_n)
+    hoa_tinh_base = [2,3,1,9]  # Dần/Ngọ/Tuất=Dần, Thân/Tý/Thìn=Mão, Tỵ/Dậu/Sửu=Sửu, Hợi/Mão/Mùi=Dậu
+    hoa_group = chi_nam_idx % 4
+    hoa_tinh_pos = (hoa_tinh_base[hoa_group] + gio_idx) % 12
+
+    # Linh Tinh theo Chi năm + giờ
+    linh_base = [10,10,3,10]
+    linh_tinh_pos = (linh_base[hoa_group] + gio_idx) % 12
+
+    # Địa Không/Địa Kiếp theo giờ
+    dia_khong_pos = (11 - gio_idx) % 12
+    dia_kiep_pos = (gio_idx + 1) % 12
+
+    # Thiên Mã theo Chi năm
+    thien_ma_map = {0:2, 1:11, 2:8, 3:5, 4:2, 5:11, 6:8, 7:5, 8:2, 9:11, 10:8, 11:5}
+    thien_ma_pos = thien_ma_map[chi_nam_idx]
+
+    # Đào Hoa theo Chi năm
+    dao_hoa_map = {0:9, 1:6, 2:3, 3:0, 4:9, 5:6, 6:3, 7:0, 8:9, 9:6, 10:3, 11:0}
+    dao_hoa_pos = dao_hoa_map[chi_nam_idx]
+
+    # Hồng Loan theo Chi năm
+    hong_loan_pos = (3 - chi_nam_idx) % 12
+    # Thiên Hỷ = Hồng Loan + 6
+    thien_hi_pos = (hong_loan_pos + 6) % 12
+
+    # Cô Thần/Quả Tú theo Chi năm
+    co_than_map = [2,2,5,5,5,8,8,8,11,11,11,2]
+    qua_tu_map = [10,10,1,1,1,4,4,4,7,7,7,10]
+    co_than_pos = co_than_map[chi_nam_idx]
+    qua_tu_pos = qua_tu_map[chi_nam_idx]
+
+    # Long Trì/Phượng Các theo Chi năm
+    long_tri_pos = (chi_nam_idx + 4) % 12
+    phuong_cac_pos = (8 - chi_nam_idx) % 12
+
+    # Thiên Khốc/Thiên Hư theo Chi năm
+    thien_khoc_pos = (6 + chi_nam_idx) % 12
+    thien_hu_pos = (6 - chi_nam_idx) % 12
+
+    # Thiên Quan/Thiên Phúc theo Can năm
+    thien_quan_map = [7,4,1,2,3,9,11,9,3,6]
+    thien_phuc_map = [9,8,0,11,3,2,6,5,6,5]
+    thien_quan_pos = thien_quan_map[can_idx]
+    thien_phuc_pos = thien_phuc_map[can_idx]
+
+    # Tang Môn/Bạch Hổ/Quan Phù/Điếu Khách theo Chi năm
+    tang_mon_pos = (chi_nam_idx + 2) % 12
+    bach_ho_pos = (chi_nam_idx + 4) % 12
+    quan_phu_pos = (chi_nam_idx + 6) % 12
+    dieu_khach_pos = (chi_nam_idx + 8) % 12
+
+    # Thai/Tọa theo tháng
+    thai_pos = (thang_am + 1) % 12
+    toa_pos = (thang_am + 11) % 12
+
+    # Trường Sinh 12 cung theo Cục
+    TRUONG_SINH_12 = ["Trường Sinh","Mộc Dục","Quan Đới","Lâm Quan","Đế Vượng",
+                      "Suy","Bệnh","Tử","Mộ","Tuyệt","Thai","Dưỡng"]
+    ts_khoi = {2:8, 3:11, 4:5, 5:8, 6:2}  # Thủy=Thân, Mộc=Hợi, Kim=Tỵ, Thổ=Thân, Hỏa=Dần
+    ts_start = ts_khoi.get(cuc, 2)
+    ts_step = 1 if duong_nam else -1
+
+    truong_sinh = {}
+    for i, ts_name in enumerate(TRUONG_SINH_12):
+        ts_pos = (ts_start + ts_step * i) % 12
+        truong_sinh[ts_name] = ts_pos
+
+    # Tổng hợp phụ tinh
     phu_tinh_list = [
         ("Lộc Tồn", loc_ton_pos), ("Kình Dương", kinh_pos), ("Đà La", da_la_pos),
         ("Văn Xương", van_xuong_pos), ("Văn Khúc", van_khuc_pos),
         ("Tả Phụ", ta_phu_pos), ("Hữu Bật", huu_bat_pos),
+        ("Thiên Khôi", thien_khoi_pos), ("Thiên Việt", thien_viet_pos),
+        ("Hỏa Tinh", hoa_tinh_pos), ("Linh Tinh", linh_tinh_pos),
+        ("Địa Không", dia_khong_pos), ("Địa Kiếp", dia_kiep_pos),
+        ("Thiên Mã", thien_ma_pos), ("Đào Hoa", dao_hoa_pos),
+        ("Hồng Loan", hong_loan_pos), ("Thiên Hỷ", thien_hi_pos),
+        ("Cô Thần", co_than_pos), ("Quả Tú", qua_tu_pos),
+        ("Long Trì", long_tri_pos), ("Phượng Các", phuong_cac_pos),
+        ("Thiên Khốc", thien_khoc_pos), ("Thiên Hư", thien_hu_pos),
+        ("Thiên Quan", thien_quan_pos), ("Thiên Phúc", thien_phuc_pos),
+        ("Tang Môn", tang_mon_pos), ("Bạch Hổ", bach_ho_pos),
+        ("Quan Phù", quan_phu_pos), ("Điếu Khách", dieu_khach_pos),
+        ("Thai", thai_pos), ("Tọa", toa_pos),
     ]
+    # Trường Sinh vào phụ tinh
+    for ts_name, ts_pos in truong_sinh.items():
+        phu_tinh_list.append((ts_name, ts_pos))
 
     for star_name, pos in phu_tinh_list:
         for cung_data in cung_map.values():
@@ -174,7 +266,6 @@ def lap_la_so(nam_sinh, thang_am, ngay_am, gio_idx, gioi_tinh='nam'):
 
     # Đại Hạn (10 năm 1 hạn)
     dai_han = []
-    duong_nam = (gioi_tinh == 'nam' and can_idx % 2 == 0) or (gioi_tinh == 'nu' and can_idx % 2 == 1)
     step = 1 if duong_nam else -1
     for i in range(12):
         start = cuc + i * 10
